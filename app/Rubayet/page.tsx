f08e18235c49b65d853e80b4368067d71aec7eca
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-06-24 13:17:57 +0600

    rubayet test commit

---
+++ app/Rubayet/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Rubayet = () => {
+    return (
+        <div className='flex flex-col h-screen justify-center'>
+            <h1 className='text-5xl text-center font-extrabold'>Rubayet Hasan Yasin </h1>
+        </div>
+    );
+};
+
+export default Rubayet;
