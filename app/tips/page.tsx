82d6148f49d72f21f57c7c1111943b91495b1883
devMorshed <ac3m0r@gmail.com>
2023-06-26 00:18:40 +0600

    tips

---
+++ app/tips/page.tsx
@@ -0,0 +1,39 @@
+
+const Local = () => {
+  return (
+		<div className="flex justify-center items-center my-10">
+			<div className="flex flex-col gap-4">
+				<p className="text-3xl">Steps to follow</p>
+				<p>1: রিপোটা ক্লোন করেন </p>
+				<code className="bg-gray-200 p-4 rounded">
+					git clone https://github.com/sadmanador/new_group_next_app
+        </code>
+        
+				<p>
+					2: নিজের ব্রাঞ্চ বানায়ে তাতে যান নইলে আগের ব্রাঞ্চে যান (-b
+					বাদ সেক্ষেত্রে){" "}
+				</p>
+				<code className="bg-gray-200 p-4 rounded">
+					git checkout -b morshed*
+				</code>
+
+				<p>3: ইচ্ছামতো কোড করতে থাকেন, তাপ্পর এ্যাড, কমিট, পুশ </p>
+				<code className="bg-gray-200 p-4 rounded">
+					git add * <br />
+					git commit -m "ম্যালা কাজ কসসি" <br />
+					git push
+				</code>
+
+				<p>4: আপডেটেড কোড নেয়ার জন্য মেইনে গিয়া টান মারেন </p>
+				<code className="bg-gray-200 p-4 rounded">
+					git checkout main <br />
+					git pull
+				</code>
+
+				<p>তাপ্পর আবার সাথে সাথে নিজের ব্রাঞ্চে যাইতে ভুইল্লেন্না - মোদ্দাকথা মেইনে থাকা যাবে না</p>
+			</div>
+		</div>
+  );
+};
+
+export default Local;
